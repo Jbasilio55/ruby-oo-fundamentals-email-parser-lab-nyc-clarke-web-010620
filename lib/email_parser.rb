fcbe782cfe19c9e80e9167ba277d84295bef083a
Jorge Basilio <j.l.basilio55@gmail.com>
2020-01-08 13:47:42 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,9 +3,13 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 class EmailAddressParser
-  attr_accessor :email
+  attr_accessor :emails
 
-  def initialize(email)
-    @email = email
+  def initialize(emails)
+    @emails = emails
   end
+end
+
+def parse
+  email.split(/, | /).uniq
 end
